741a30f17542545c684f77f4e41d105ffb04fbc3
Flavio Monteiro <flaviohnm@gmail.com>
2023-08-10 22:31:12 -0300

    feat: implementacao de teste de comparacao e ordenacao de Array

---
+++ src/test/java/br/com/monteiro/ArraysCompareTest.java
@@ -0,0 +1,20 @@
+package br.com.monteiro;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class ArraysCompareTest {
+
+    @Test
+    void test() {
+        int[] numbers = {25, 8, 21, 32, 3};
+        int[] expectedArray = {3, 8, 21, 25, 32};
+
+        Arrays.sort(numbers);
+
+        assertArrayEquals(numbers, expectedArray);
+    }
+}
