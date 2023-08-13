8cf4b271aead6e5562cd220b68fbad3d60caf61d
Flavio Monteiro <flaviohnm@gmail.com>
2023-08-13 19:26:45 -0300

    feat: implementacao dos tipos de ordenacao de testes relacionadaos ao ciclo de vida

---
+++ src/test/java/br/com/monteiro/MethodOrderedByNameTest.java
@@ -0,0 +1,29 @@
+package br.com.monteiro;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@Order(2)
+@TestMethodOrder(MethodOrderer.MethodName.class)
+public class MethodOrderedByNameTest {
+
+    @Test
+    void testD(){
+        System.out.println("Running Test D");
+    }
+    @Test
+    void testB(){
+        System.out.println("Running Test B");
+    }
+    @Test
+    void testC(){
+        System.out.println("Running Test C");
+    }
+    @Test
+    void testA(){
+        System.out.println("Running Test A");
+    }
+
+}
