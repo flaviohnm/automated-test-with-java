d05536f6a7131389e5371ba50edb50d307cea9d6
Flavio Monteiro <flaviohnm@gmail.com>
2023-08-21 09:27:25 -0300

    feat: adicionar recursos avancados com mockito

---
+++ src/test/java/br/com/monteiro/mockito/SpyTest.java
@@ -0,0 +1,56 @@
+package br.com.monteiro.mockito;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class SpyTest {
+
+    @Test
+    void testV1(){
+        List<String> mockArrayList = mock(ArrayList.class);
+
+        assertEquals(0, mockArrayList.size());
+
+        when(mockArrayList.size()).thenReturn(5);
+        mockArrayList.add("Foo-Bar");
+
+        assertEquals(5, mockArrayList.size());
+    }
+
+    @Test
+    void testV2(){
+        List<String> spyArrayList = spy(ArrayList.class);
+
+        assertEquals(0, spyArrayList.size());
+
+        spyArrayList.add("Foo-Bar");
+        assertEquals(1, spyArrayList.size());
+
+        spyArrayList.remove("Foo-Bar");
+        assertEquals(0, spyArrayList.size());
+    }
+
+    @Test
+    void testV3(){
+        List<String> spyArrayList = spy(ArrayList.class);
+        assertEquals(0, spyArrayList.size());
+        when(spyArrayList.size()).thenReturn(5);
+        assertEquals(5, spyArrayList.size());
+    }
+
+    @Test
+    void testV4(){
+        List<String> spyArrayList = spy(ArrayList.class);
+        assertEquals(0, spyArrayList.size());
+        spyArrayList.add("Foo-Bar");
+        verify(spyArrayList).add("Foo-Bar");
+        verify(spyArrayList, never()).remove("Foo-Bar");
+        verify(spyArrayList, never()).remove(anyString());
+        verify(spyArrayList, never()).clear();
+    }
+}
